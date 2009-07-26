9a13f769cdb0418b0107db173150412c39b4907d
Nick Stenning <nick@whiteink.com>
2009-07-26 12:00:33 +0100

    First few tests. These can either be run in-browser (visit spec/fixtures/annotator.html) or from the console: run 'rake'

--- spec/annotator_spec.js
+++ spec/annotator_spec.js
@@ -3,14 +3,36 @@ require("../js/jqext.js");
 require("../js/annotator.js");
 
 Screw.Unit(function() {
-  describe("Your application javascript", function() {
-    it("does something", function() {
-      expect("hello").to(equal, "hello");
-    });
+    describe("Annotator object", function() {
 
-    it("accesses the DOM from fixtures/application.html", function() {
-      expect($('.select_me').length).to(equal, 2);
+        var a,
+            node = $('p').textNodes().get(0), 
+            range = {
+                startContainer: node,
+                startOffset: 0,
+                endContainer: node,
+                endOffset: 27
+            },
+            sel = { getRangeAt: function (i) { return range; } };
+
+        before(function () {
+            a = new Annotator();
+            stub(window, 'getSelection').and_return(sel);
+        });
+        
+        it("checks to see if a selection has been made on mouseup", function() {
+            expect(a.range).to(equal, null);
+            $('body').mouseup();
+            expect(a.range).to(equal, range);
+        });
+
+        it("highlights the selected text when the annotate icon is clicked on", function() {
+            expect($(node.parentNode)).to(match_selector, "strong");
+            expect(node.nodeValue.length).to(equal, 37);
+            $('#noteLink img').mousedown();
+            expect($(node.nextSibling)).to(match_selector, "span.hilight");
+            expect(node.nodeValue.length).to(equal, 0);
+        });
     });
-  });
 });
 
