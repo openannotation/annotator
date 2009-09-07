184e3f01049d3ef6ea3369b80d48e81ff1eb914d
Nick Stenning <nick@whiteink.com>
2009-09-07 12:13:28 +0100

    Upgrade jspec. Remove rhino spec for the moment. Use server spec for checking compressed version.

--- spec/jspec/jspec.xhr.js
+++ spec/jspec/jspec.xhr.js
@@ -16,12 +16,12 @@
   }
   
   MockXMLHttpRequest.prototype = {
-    status : 0,
-    async : true,
-    readyState : 0,
-    responseText : '',
-    abort : function(){},
-    onreadystatechange : function(){},
+    status: 0,
+    async: true,
+    readyState: 0,
+    responseText: '',
+    abort: function(){},
+    onreadystatechange: function(){},
 
    /**
     * Return response headers hash.
@@ -77,47 +77,47 @@
   // --- Response status codes
   
   JSpec.statusCodes = {
-    100 : 'Continue',
-    101 : 'Switching Protocols',
-    200 : 'OK',
-    201 : 'Created',
-    202 : 'Accepted',
-    203 : 'Non-Authoritative Information',
-    204 : 'No Content',
-    205 : 'Reset Content',
-    206 : 'Partial Content',
-    300 : 'Multiple Choice',
-    301 : 'Moved Permanently',
-    302 : 'Found',
-    303 : 'See Other',
-    304 : 'Not Modified',
-    305 : 'Use Proxy',
-    307 : 'Temporary Redirect',
-    400 : 'Bad Request',
-    401 : 'Unauthorized',
-    402 : 'Payment Required',
-    403 : 'Forbidden',
-    404 : 'Not Found',
-    405 : 'Method Not Allowed',
-    406 : 'Not Acceptable',
-    407 : 'Proxy Authentication Required',
-    408 : 'Request Timeout',
-    409 : 'Conflict',
-    410 : 'Gone',
-    411 : 'Length Required',
-    412 : 'Precondition Failed',
-    413 : 'Request Entity Too Large',
-    414 : 'Request-URI Too Long',
-    415 : 'Unsupported Media Type',
-    416 : 'Requested Range Not Satisfiable',
-    417 : 'Expectation Failed',
-    422 : 'Unprocessable Entity',
-    500 : 'Internal Server Error',
-    501 : 'Not Implemented',
-    502 : 'Bad Gateway',
-    503 : 'Service Unavailable',
-    504 : 'Gateway Timeout',
-    505 : 'HTTP Version Not Supported'
+    100: 'Continue',
+    101: 'Switching Protocols',
+    200: 'OK',
+    201: 'Created',
+    202: 'Accepted',
+    203: 'Non-Authoritative Information',
+    204: 'No Content',
+    205: 'Reset Content',
+    206: 'Partial Content',
+    300: 'Multiple Choice',
+    301: 'Moved Permanently',
+    302: 'Found',
+    303: 'See Other',
+    304: 'Not Modified',
+    305: 'Use Proxy',
+    307: 'Temporary Redirect',
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    402: 'Payment Required',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Not Allowed',
+    406: 'Not Acceptable',
+    407: 'Proxy Authentication Required',
+    408: 'Request Timeout',
+    409: 'Conflict',
+    410: 'Gone',
+    411: 'Length Required',
+    412: 'Precondition Failed',
+    413: 'Request Entity Too Large',
+    414: 'Request-URI Too Long',
+    415: 'Unsupported Media Type',
+    416: 'Requested Range Not Satisfiable',
+    417: 'Expectation Failed',
+    422: 'Unprocessable Entity',
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+    502: 'Bad Gateway',
+    503: 'Service Unavailable',
+    504: 'Gateway Timeout',
+    505: 'HTTP Version Not Supported'
   }
   
   /**
@@ -136,10 +136,10 @@
       headers = headers || {}
       headers['content-type'] = type
       JSpec.extend(XMLHttpRequest.prototype, {
-        responseText : body,
-        responseHeaders : headers,
-        status : status,
-        statusText : JSpec.statusCodes[status]
+        responseText: body,
+        responseHeaders: headers,
+        status: status,
+        statusText: JSpec.statusCodes[status]
       })
     }}
   }
@@ -155,18 +155,28 @@
   }
   
   JSpec.include({
+    name: 'Mock XHR',
 
     // --- Utilities
 
     utilities : {
-      mockRequest : mockRequest,
-      unmockRequest : unmockRequest
+      mockRequest: mockRequest,
+      unmockRequest: unmockRequest
     },
 
     // --- Hooks
 
     afterSpec : function() {
       this.utilities.unmockRequest()
+    },
+    
+    // --- DSLs
+    
+    DSLs : {
+      snake : {
+        mock_request: mockRequest,
+        unmock_request: unmockRequest
+      }
     }
 
   })
