a27a817b17ac6c948a1eb835d593569af6f7bb1e
Aron Carroll <self@aroncarroll.com>
2011-03-04 10:48:41 +0000

    Updated the Bookmarklet to be testable

--- contrib/bookmarklet/src/bookmarklet.js
+++ contrib/bookmarklet/src/bookmarklet.js
@@ -2,10 +2,8 @@
 
   var body = document.body,
       head = document.getElementsByTagName('head')[0],
-      _Annotator, notification;
-
-  // Cache any existing annotator.
-  _Annotator = window.Annotator;
+      bookmarklet = {},
+      notification;
 
   notification = (function () {
     var element = document.createElement('div'),
@@ -84,130 +82,142 @@
     };
   }());
 
-  function keypath(object, path, fallback) {
-    var keys = (path || '').split('.'),
-        key;
+  bookmarklet = {
+    notification: notification,
 
-    while (object && keys.length) {
-      key = keys.shift();
+    keypath: function (object, path, fallback) {
+      var keys = (path || '').split('.'),
+          key;
 
-      if (object.hasOwnProperty(key)) {
-        object = object[key];
+      while (object && keys.length) {
+        key = keys.shift();
 
-        if (keys.length === 0 && object !== undefined) {
-          return object;
+        if (object.hasOwnProperty(key)) {
+          object = object[key];
+
+          if (keys.length === 0 && object !== undefined) {
+            return object;
+          }
+        } else {
+          break;
         }
+      }
+
+      return (fallback == null) ? null : fallback;
+    },
+
+    config: function (path, fallback) {
+      var value = this.keypath(options, path, fallback);
+
+      if (value === null) {
+        notification.show(
+          'Sorry there was an error reading the bookmarklet setting for key: ' + path,
+          notification.status.ERROR
+        );
+        setTimeout(notification.hide, 3000);
+      }
+
+      return value;
+    },
+
+    loadjQuery: function () {
+      var script = document.createElement('script');
+
+      script.src = config('externals.jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js');
+      script.onload = function () {
+        jQuery = window.jQuery;
+
+        body.removeChild(script);
+        notification.load(function () {
+          jQuery.noConflict(true);
+          setup();
+        });
+      };
+
+      body.appendChild(script);
+    },
+
+    load: function (callback) {
+      head.appendChild($('<link />', {
+        rel: 'stylesheet',
+        href: config('externals.styles')
+      })[0]);
+
+      jQuery.getScript(config('externals.source'), callback);
+    },
+
+    setup: function () {
+      var annotator = jQuery(options.target || body).annotator().data('annotator'),
+          uri = location.href.split(/#|\?/).shift();
+
+      annotator
+        .addPlugin('Unsupported')
+        .addPlugin('Store', {
+          prefix: config('store.prefix'),
+          annotationData: {
+            'uri': uri
+          },
+          loadFromSearch: {
+            'uri': uri,
+            'all_fields': 1
+          }
+        })
+        .addPlugin('Permissions', {
+          user: config('permissions.user'),
+          permissions: config('permissions.permissions'),
+          userId: function (user) {
+            return user ? user.id : '';
+          },
+          userString: function (user) {
+            return user ? user.name : '';
+          }
+        })
+        // As we're not requesting the auth tokens for the bookmarklet we
+        // don't need the Auth plugin. Instead we just need to set the required
+        // headers on each request.
+        .element.data('annotator:headers', config('auth.headers'));
+
+      // Attach the annotator to the window object so we can prevent it
+      // being loaded twice and test.
+      jQuery.extend(window._annotator, {
+        jQuery: jQuery,
+        element: body,
+        instance: annotator,
+        Annotator: Annotator.noConflict()
+      });
+
+      notification.message('Annotator is ready!', notification.status.SUCCESS);
+      setTimeout(function () {
+        notification.hide();
+        setTimeout(notification.remove, 800);
+      }, 3000);
+    },
+
+    init: function () {
+      if (window._annotator.instance) {
+        window._annotator.Annotator.showNotification(
+          'Annotator is already loaded. Try highlighting some text to get started'
+        );
       } else {
-        break;
+        notification.show('Loading Annotator into page');
+        console.log(window.jQuery);
+        if (window.jQuery === undefined || !window.jQuery.sub) {
+          this.loadjQuery();
+        } else {
+          jQuery = window.jQuery.sub();
+          this.load(this.setup);
+        }
       }
     }
+  };
 
-    return (fallback == null) ? null : fallback;
+  // Export the bookmarklet to the window object for testing.
+  window._annotator = {
+    bookmarklet: bookmarklet
+  };
+
+  // Load the bookmarklet.
+  if (!options.test) {
+    init();
   }
-
-  function config(path, fallback) {
-    var value = keypath(options, path, fallback);
-
-    if (value === null) {
-      notification.show(
-        'Sorry there was an error reading the bookmarklet setting for key: ' + path,
-        notification.status.ERROR
-      );
-      setTimeout(notification.hide, 3000);
-    }
-
-    return value;
-  }
-
-  function loadjQuery() {
-    var script = document.createElement('script');
-
-    script.src = config('externals.jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js');
-    script.onload = function () {
-      jQuery = window.jQuery;
-
-      body.removeChild(script);
-      load(function () {
-        jQuery.noConflict(true);
-        setup();
-      });
-    };
-
-    body.appendChild(script);
-  }
-
-  function load(callback) {
-    head.appendChild($('<link />', {
-      rel: 'stylesheet',
-      href: config('externals.styles')
-    })[0]);
-
-    jQuery.getScript(config('externals.source'), callback);
-  }
-
-  function setup() {
-    var annotator = jQuery(body).annotator().data('annotator'),
-        uri = location.href.split(/#|\?/).shift();
-
-    annotator
-      .addPlugin('Unsupported')
-      .addPlugin('Store', {
-        prefix: config('store.prefix'),
-        annotationData: {
-          'uri': uri
-        },
-        loadFromSearch: {
-          'uri': uri,
-          'all_fields': 1
-        }
-      })
-      .addPlugin('Permissions', {
-        user: config('permissions.user'),
-        permissions: config('permissions.permissions'),
-        userId: function (user) {
-          return user ? user.id : '';
-        },
-        userString: function (user) {
-          return user ? user.name : '';
-        }
-      })
-      // As we're not requesting the auth tokens for the bookmarklet we
-      // don't need the Auth plugin. Instead we just need to set the required
-      // headers on each request.
-      .element.data('annotator:headers', config('auth.headers'));
-
-    // Attach the annotator to the window object so we can prevent it
-    // being loaded twice.
-    window._annotator = {
-      jQuery: jQuery,
-      element: body,
-      instance: annotator,
-      Annotator: annotator.constructor
-    };
-
-    // Re-assign the original Annotator back to its rightful place.
-    window.Annotator = _Annotator;
-
-    notification.message('Annotator is ready!', notification.status.SUCCESS);
-    setTimeout(function () {
-      notification.hide();
-      setTimeout(notification.remove, 800);
-    }, 3000);
-  }
-
-  if (window._annotator) {
-    window._annotator.Annotator.showNotification(
-      'Annotator is already loaded. Try highlighting some text to get started'
-    );
-  } else {
-    notification.show('Loading Annotator into page');
-
-    if (jQuery === undefined || !jQuery.sub) {
-      loadjQuery();
-    } else {
-      jQuery = jQuery.sub();
-      load(setup);
-    }
-  }
-
 }(__config__, this, this.document, this.jQuery));
