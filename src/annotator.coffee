openannotation/annotator

commit 033c012c98a2078f7ebc6c92a92ebeed4e12c3d9

Add a temporary highlight to the document to show the user what they selected. Closes #123.

src/annotator.coffee
@@ -45,7 +45,6 @@ class Annotator extends Delegator
     ".annotator-hl mouseout":            "startViewerHideTimer"
 
   html:
-    hl:      '<span class="annotator-hl"></span>'
     adder:   '<div class="annotator-adder"><button>' + _t('Annotate') + '</button></div>'
     wrapper: '<div class="annotator-wrapper"></div>'
 
@@ -99,9 +98,8 @@ class Annotator extends Delegator
     this._setupWrapper()._setupViewer()._setupEditor()
     this._setupDynamicStyle()
 
-    # Create model dom elements
-    for name, src of @html
-      this[name] = $(src).appendTo(@wrapper).hide() unless name == 'wrapper'
+    # Create adder
+    this.adder = $(this.html.adder).appendTo(@wrapper).hide()
 
   # Wraps the children of @element in a @wrapper div. NOTE: This method will also
   # remove any script elements inside @element to prevent them re-executing.
@@ -396,14 +394,17 @@ class Annotator extends Delegator
     else
       console.warn(_t("Can't dump annotations without Store plugin."))
 
-  # Public: Wraps the DOM Nodes within the provided range in the @hl wrapper
-  # and returns the highlight Elements.
+  # Public: Wraps the DOM Nodes within the provided range with a highlight
+  # element of the specified class and returns the highlight Elements.
   #
   # normedRange - A NormalizedRange to be highlighted.
+  # cssClass - A CSS class to use for the highlight (default: 'annotator-hl')
   #
   # Returns an array of highlight Elements.
-  highlightRange: (normedRange) ->
+  highlightRange: (normedRange, cssClass='annotator-hl') ->
     white = /^\s*$/
+
+    hl = $("<span class='#{cssClass}'></span>")
 
     # Ignore text nodes that contain only whitespace characters. This prevents
     # spans being injected between elements that can only contain a restricted
@@ -411,7 +412,19 @@ class Annotator extends Delegator
     # may be the odd abandoned whitespace node in a paragraph that is skipped
     # but better than breaking table layouts.
     for node in normedRange.textNodes() when not white.test(node.nodeValue)
-      $(node).wrapAll(@hl).parent().show()[0]
+      $(node).wrapAll(hl).parent().show()[0]
+
+  # Public: highlight a list of ranges
+  #
+  # normedRanges - An array of NormalizedRanges to be highlighted.
+  # cssClass - A CSS class to use for the highlight (default: 'annotator-hl')
+  #
+  # Returns an array of highlight Elements.
+  highlightRanges: (normedRanges, cssClass='annotator-hl') ->
+    highlights = []
+    for r in normedRanges
+      $.merge highlights, this.highlightRange(r, cssClass)
+    highlights
 
   # Public: Registers a plugin with the Annotator. A plugin can only be
   # registered once. The plugin will be instantiated in the following order.
@@ -624,8 +637,18 @@ class Annotator extends Delegator
   onAdderClick: (event) =>
     event?.preventDefault()
 
+    # Hide the adder
     position = @adder.position()
     @adder.hide()
+
+    # Show a temporary highlight so the user can see what they selected
+    if @selectedRanges and @selectedRanges.length
+      ranges = (Range.sniff(r).normalize() for r in @selectedRanges)
+      highlights = this.highlightRanges(ranges, 'annotator-hl-temporary')
+
+      @editor.element.one 'hide', ->
+        for h in highlights
+          $(h).replaceWith(h.childNodes)
 
     # Create an annotation and display the editor.
     this.showEditor(this.createAnnotation(), position)

test/spec/annotator_spec.coffee
@@ -47,10 +47,9 @@ describe 'Annotator', ->
       Annotator.prototype.constructor.call(annotator, annotator.element[0])
       expect(annotator.hasOwnProperty('plugins')).toBeTruthy()
 
-    it "should create the adder and highlight properties from the @html strings", ->
+    it "should create the adder properties from the @html strings", ->
       Annotator.prototype.constructor.call(annotator, annotator.element[0])
       expect(annotator.adder instanceof $).toBeTruthy()
-      expect(annotator.hl instanceof $).toBeTruthy()
 
     it "should call Annotator#_setupWrapper()", ->
       Annotator.prototype.constructor.call(annotator, annotator.element[0])
@@ -516,6 +515,32 @@ describe 'Annotator', ->
       expect(elements[0].className).toBe('annotator-hl')
       expect(elements[0].firstChild).toBe(textNodes[0])
 
+    it "should set highlight element class names to its second argument", ->
+      textNodes = (document.createTextNode(text) for text in ['hello', 'world'])
+      mockRange =
+        textNodes: -> textNodes
+
+      elements = annotator.highlightRange(mockRange, 'monkeys')
+      expect(elements[0].className).toBe('monkeys')
+
+  describe "highlightRanges", ->
+    it "should return a list of highlight elements all highlighted ranges", ->
+      textNodes = (document.createTextNode(text) for text in ['hello', 'world'])
+      mockRange =
+        textNodes: -> textNodes
+      ranges = [mockRange, mockRange, mockRange]
+      elements = annotator.highlightRanges(ranges)
+      expect(elements.length).toBe(6)
+      expect(elements[0].className).toBe('annotator-hl')
+
+    it "should set highlight element class names to its second argument", ->
+      textNodes = (document.createTextNode(text) for text in ['hello', 'world'])
+      mockRange =
+        textNodes: -> textNodes
+      ranges = [mockRange, mockRange, mockRange]
+      elements = annotator.highlightRanges(ranges, 'monkeys')
+      expect(elements[0].className).toBe('monkeys')
+
   describe "addPlugin", ->
     plugin = null
 
@@ -803,6 +828,9 @@ describe 'Annotator', ->
       spyOn(annotator.adder, 'position').andReturn(mockOffset)
       spyOn(annotator, 'createAnnotation').andReturn(annotation)
       spyOn(annotator, 'showEditor')
+      spyOn(Range, 'sniff').andReturn({ normalize: -> 'normalized' })
+      spyOn(annotator, 'highlightRanges').andReturn(['baz', 'bat'])
+      annotator.selectedRanges = ['foo', 'bar']
 
       annotator.onAdderClick()
 
@@ -815,6 +843,9 @@ describe 'Annotator', ->
     it "should display the Annotation#editor in the same place as the Annotation#adder", ->
       expect(annotator.adder.position).toHaveBeenCalled()
       expect(annotator.showEditor).toHaveBeenCalledWith(annotation, mockOffset)
+
+    it "should add temporary highlights to the document to show the user what they selected", ->
+      expect(annotator.highlightRanges).toHaveBeenCalledWith(['normalized', 'normalized'], 'annotator-hl-temporary')
 
   describe "onEditAnnotation", ->
     it "should display the Annotator#editor in the same positions as Annotatorviewer", ->

css/annotator.css
@@ -47,6 +47,10 @@
 	background: rgba(255, 255, 10, 0.3);
 }
 
+.annotator-hl-temporary {
+	background: rgba(0, 124, 255, 0.3);
+}
+
 /* Annotator Wrapper
 -------------------------------------------------------------------- */
 

commit 3ce1fdb0b0696c1252fb4b1a02e379dc8987ef1e

Use 'annotator-hl' class for temporary highlights.
- Fix for bug introduced by 033c012c98a2078f7ebc6c92a92ebeed4e12c3d9

- Spotted by Eric Collins (@tabfugnic) The "span" for the temporary
  highlight wasn't being ignored by Range.NormalizedRange#serialize()
  and was being included in the submitted XPath for the annotation,
  resulting in a failure to redraw the annotation on page load

Signed-off-by: Nick Stenning <nick@whiteink.com>

src/annotator.coffee
@@ -647,7 +647,7 @@ class Annotator extends Delegator
     # Show a temporary highlight so the user can see what they selected
     if @selectedRanges and @selectedRanges.length
       ranges = (Range.sniff(r).normalize() for r in @selectedRanges)
-      highlights = this.highlightRanges(ranges, 'annotator-hl-temporary')
+      highlights = this.highlightRanges(ranges, 'annotator-hl annotator-hl-temporary')
 
       @editor.element.one 'hide', ->
         for h in highlights

test/spec/annotator_spec.coffee
@@ -853,7 +853,7 @@ describe 'Annotator', ->
       expect(annotator.showEditor).toHaveBeenCalledWith(annotation, mockOffset)
 
     it "should add temporary highlights to the document to show the user what they selected", ->
-      expect(annotator.highlightRanges).toHaveBeenCalledWith(['normalized', 'normalized'], 'annotator-hl-temporary')
+      expect(annotator.highlightRanges).toHaveBeenCalledWith(['normalized', 'normalized'], 'annotator-hl annotator-hl-temporary')
 
   describe "onEditAnnotation", ->
     it "should display the Annotator#editor in the same positions as Annotatorviewer", ->