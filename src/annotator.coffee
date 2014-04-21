openannotation/annotator

commit 1336fbbab0c3d45a98c9fe5f581b79c6bcffdf64

Fix legacy plugin hash population in #configure()
We need to compare the identity of the plugin constructor with the
registered constructors, rather than checking whether the plugin
object is an instance of the class. The reason is that the latter
gives false positives when plugins inherit from other registered
plugins.

src/annotator.coffee
@@ -124,8 +124,10 @@ class Annotator extends Delegator
     # TODO: Stop using this hash to find plugins
     # This block is super hacky and dumb.
     for p in plugins
-      for name, klass of Annotator.Plugin._ctors when p instanceof klass
-        @plugins[name] = p
+      for name, klass of Annotator.Plugin._ctors
+        if p.constructor is klass
+          @plugins[name] = p
+          break
 
     @annotations = new AnnotationRegistry()
     @annotations.configure(core: this)

commit acb49e23583eedfc0abdf688d551ff4053513dd0

Fix broken Annotator#destroy() introduced by 1336fbb
Annotator.plugins is currently an object, not an array

src/annotator.coffee
@@ -199,8 +199,8 @@ class Annotator extends Delegator
   destroy: ->
     $('#annotator-dynamic-style').remove()
 
-    for plugin in @plugins
-      plugin.destroy()
+    for name, plugin of @plugins
+      plugin.destroy?()
 
     this.removeEvents()
     idx = Annotator._instances.indexOf(this)