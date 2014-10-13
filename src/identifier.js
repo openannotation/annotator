82cac07561a57b60f61eb9510d886a3a4fc13ceb
Nick Stenning <nick@whiteink.com>
2014-10-13 12:37:54 +0200

    identifier: Small documentation tweaks

--- src/identifier.js
+++ src/identifier.js
@@ -3,7 +3,6 @@
 /**
  * Default identifier
  *
- * @constructor
  * @param {*} identity The identity to report as the current user
  */
 function Default(identity) {
--- test/spec/identifier_spec.js
+++ test/spec/identifier_spec.js
@@ -3,7 +3,7 @@ var Identifier = require('../../src/identifier');
 
 describe('Identifier.Default', function () {
     describe('#who()', function () {
-        it('returns the identity passed to the constructor', function () {
+        it('returns the identity passed to the factory', function () {
             var identifier = Identifier.Default('alice')();
             assert.equal(identifier.who(), 'alice');
         });
