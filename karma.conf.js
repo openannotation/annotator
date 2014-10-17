2795d9adbfa774d25291794897102f7b4169d69f
Nick Stenning <nick@whiteink.com>
2014-10-17 16:36:05 +0200

    Enable testing on browsers provided by Sauce Labs
    This enables testing against multiple browsers using Travis and Sauce
    Connect.

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,66 @@ module.exports = function (karma) {
             }
         },
 
-        browsers: ['PhantomJS']
+        browsers: ['PhantomJS'],
+
+        reporters: ['dots'],
+
+        customLaunchers: {
+            'SL_Chrome': {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                version: '38'
+            },
+            'SL_Firefox': {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                version: '33'
+            },
+            'SL_Safari': {
+                base: 'SauceLabs',
+                browserName: 'safari',
+                platform: 'OS X 10.9',
+                version: '7'
+            },
+            'SL_IE_8': {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                platform: 'Windows 7',
+                version: '8'
+            },
+            'SL_IE_9': {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                platform: 'Windows 7',
+                version: '9'
+            },
+            'SL_IE_10': {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                platform: 'Windows 8',
+                version: '10'
+            },
+            'SL_IE_11': {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                platform: 'Windows 8.1',
+                version: '11'
+            }
+        },
+
+        sauceLabs: {
+            testName: 'Annotator'
+        }
     });
+
+    if (process.env.TRAVIS) {
+        var buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
+
+        karma.sauceLabs.build = buildLabel;
+        karma.sauceLabs.startConnect = false;
+        karma.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+
+        karma.browsers = ['PhantomJS', 'SL_Chrome', 'SL_Firefox', 'SL_Safari'];
+        karma.reporters = ['dots', 'saucelabs'];
+    }
 };
