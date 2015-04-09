openannotation/annotator

commit e7ad23650d90de0979e5cb20d586ac91ede75aca

Fix drawAll in Highlighter plugin
Needed results array, not the wrapping object.

src/storage.js
@@ -485,8 +485,8 @@ StorageAdapter.prototype.query = function (query) {
 StorageAdapter.prototype.load = function (query) {
     var self = this;
     return this.query(query)
-        .then(function (result) {
-            self.runHook('onAnnotationsLoaded', [result]);
+        .then(function (data) {
+            self.runHook('onAnnotationsLoaded', [data.results]);
         });
 };
 

commit befb0b9a6fac640651e92712e0312f5157124b11

Fix StorageAdapter tests (broken by e7ad236)

src/storage.js
@@ -44,7 +44,7 @@ function DebugStorage () {
 
         'query': function (queryObj) {
             trace('query', queryObj);
-            return {results: [], metadata: {total: 0}};
+            return {results: [], meta: {total: 0}};
         }
     };
 }
@@ -142,7 +142,7 @@ HTTPStorageImpl.prototype.query = function (queryObj) {
         .done(function (obj) {
             var rows = obj.rows;
             delete obj.rows;
-            dfd.resolve({results: rows, metadata: obj});
+            dfd.resolve({results: rows, meta: obj});
         })
         .fail(function () {
             dfd.reject.apply(dfd, arguments);

test/spec/storage/storageadapter_spec.js
@@ -43,7 +43,7 @@ MockStorage.prototype["delete"] = function (annotation) {
 
 MockStorage.prototype.query = function () {
     this._record('query');
-    return [[], {total: 0}];
+    return {results: [{id: 'foo'}], meta: {total: 1}};
 };
 
 MockStorage.prototype._record = function (name) {
@@ -340,7 +340,7 @@ describe('Storage.StorageAdapter', function () {
             };
             a.query(query)
                 .then(function (ret) {
-                    assert.deepEqual(ret, [[], {total: 0}]);
+                    assert.deepEqual(ret, {results: [{id: 'foo'}], meta: {total: 1}});
                 })
                 .then(done, done);
         });
@@ -383,7 +383,7 @@ describe('Storage.StorageAdapter', function () {
                 .then(function () {
                     assert.deepEqual(hr.calls[0].name, 'query');
                     assert.deepEqual(hr.calls[1].name, 'onAnnotationsLoaded');
-                    assert.deepEqual(hr.calls[1].args, [[[], {total: 0}]]);
+                    assert.deepEqual(hr.calls[1].args, [[{id: 'foo'}]]);
                 })
                 .then(done, done);
         });