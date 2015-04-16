0e9f3a16cc90a8dc8bd1c1954d8f4566d9c32f68
Nick Stenning <nick@whiteink.com>
2015-04-16 18:50:48 +0200

    Move annotator.plugin.filter -> annotator.ui.filter.standalone

--- test/spec/ui/filter_spec.js
+++ test/spec/ui/filter_spec.js
@@ -402,3 +402,52 @@ describe('ui.filter.Filter', function () {
         });
     });
 });
+
+
+describe('ui.filter.standalone', function () {
+    var mockFilter = null,
+        plugin = null,
+        sandbox = null;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        mockFilter = {
+            updateHighlights: sandbox.stub(),
+            destroy: sandbox.stub()
+        };
+
+        sandbox.stub(filter, 'Filter').returns(mockFilter);
+
+        plugin = filter.standalone();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    var hooks = [
+        'annotationsLoaded',
+        'annotationCreated',
+        'annotationUpdated',
+        'annotationDeleted'
+    ];
+
+    function testHook(h) {
+        return function () {
+            plugin[h]({text: 123});
+            sinon.assert.calledWith(mockFilter.updateHighlights);
+        };
+    }
+
+    for (var i = 0, len = hooks.length; i < len; i++) {
+        it(
+            "calls updateHighlights on the filter component " + hooks[i],
+            testHook(hooks[i])
+        );
+    }
+
+    it('destroys the filter component when destroyed', function () {
+        plugin.destroy();
+        sinon.assert.calledOnce(mockFilter.destroy);
+    });
+});
