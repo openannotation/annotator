95345b8aae95f1d6339182df4e8e8a23aa26ba36
Nick Stenning <nick@whiteink.com>
2015-04-16 13:58:42 +0200

    Sort annotator.ui imports

--- src/ui.js
+++ src/ui.js
@@ -2,10 +2,11 @@ exports.Adder = require('./ui/adder').Adder;
 exports.Editor = require('./ui/editor').Editor;
 exports.Filter = require('./ui/filter').Filter;
 exports.Highlighter = require('./ui/highlighter').Highlighter;
-exports.markdown = require('./ui/markdown').markdown;
-exports.tags = require('./ui/tags').tags;
 exports.TextSelector = require('./ui/textselector').TextSelector;
 exports.Viewer = require('./ui/viewer').Viewer;
 exports.Widget = require('./ui/widget').Widget;
+
+exports.markdown = require('./ui/markdown').markdown;
+exports.tags = require('./ui/tags').tags;
 
 exports.main = require('./ui/main').main;
