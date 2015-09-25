a909f0681d439cd00733afb697bbf8165d34cd36
Randall Leeds <randall.leeds@gmail.com>
2015-09-25 01:29:37 -0700

    Don't use core-js core in tests
    Nothing in this codebase relies on features shimmed by this.

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,6 @@ module.exports = function (karma) {
         browserify: {
             debug: true,
             configure: function (bundle) {
-              bundle.require('core-js/client/core');  // PhantomJS < 2
               bundle.require('.', {expose: 'annotator'});
             }
         },
