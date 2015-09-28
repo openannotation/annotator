b7a07b16b28b6575f8445d88e70bffd700c1f820
Yifan Ning <ningyifan1212@163.com>
2015-09-28 14:09:05 -0400

    rename test to demo, add ddiEditor to explore templating editor.js

--- src/ui/ddiEditor.js
+++ src/ui/ddiEditor.js
@@ -1,230 +1,19 @@
-"use strict";
-
 var Widget = require('./widget').Widget,
     util = require('../util');
+
+var Editor = require('./editor').Editor
 
 var $ = util.$;
 var _t = util.gettext;
 var Promise = util.Promise;
 
-var NS = "annotator-editor";
-
-
-// id returns an identifier unique within this session
-var id = (function () {
-    var counter;
-    counter = -1;
-    return function () {
-        return counter += 1;
-    };
-}());
-
-
-// preventEventDefault prevents an event's default, but handles the condition
-// that the event is null or doesn't have a preventDefault function.
-function preventEventDefault(event) {
-    if (typeof event !== 'undefined' &&
-        event !== null &&
-        typeof event.preventDefault === 'function') {
-        event.preventDefault();
-    }
-}
-
-
-// dragTracker is a function which allows a callback to track changes made to
-// the position of a draggable "handle" element.
-//
-// handle - A DOM element to make draggable
-// callback - Callback function
-//
-// Callback arguments:
-//
-// delta - An Object with two properties, "x" and "y", denoting the amount the
-//         mouse has moved since the last (tracked) call.
-//
-// Callback returns: Boolean indicating whether to track the last movement. If
-// the movement is not tracked, then the amount the mouse has moved will be
-// accumulated and passed to the next mousemove event.
-//
-var dragTracker = exports.dragTracker = function dragTracker(handle, callback) {
-    var lastPos = null,
-        throttled = false;
-
-    // Event handler for mousemove
-    function mouseMove(e) {
-        if (throttled || lastPos === null) {
-            return;
-        }
-
-        var delta = {
-            y: e.pageY - lastPos.top,
-            x: e.pageX - lastPos.left
-        };
-
-        var trackLastMove = true;
-        // The callback function can return false to indicate that the tracker
-        // shouldn't keep updating the last position. This can be used to
-        // implement "walls" beyond which (for example) resizing has no effect.
-        if (typeof callback === 'function') {
-            trackLastMove = callback(delta);
-        }
-
-        if (trackLastMove !== false) {
-            lastPos = {
-                top: e.pageY,
-                left: e.pageX
-            };
-        }
-
-        // Throttle repeated mousemove events
-        throttled = true;
-        setTimeout(function () { throttled = false; }, 1000 / 60);
-    }
-
-    // Event handler for mouseup
-    function mouseUp() {
-        lastPos = null;
-        $(handle.ownerDocument)
-            .off('mouseup', mouseUp)
-            .off('mousemove', mouseMove);
-    }
-
-    // Event handler for mousedown -- starts drag tracking
-    function mouseDown(e) {
-        if (e.target !== handle) {
-            return;
-        }
-
-        lastPos = {
-            top: e.pageY,
-            left: e.pageX
-        };
-
-        $(handle.ownerDocument)
-            .on('mouseup', mouseUp)
-            .on('mousemove', mouseMove);
-
-        e.preventDefault();
-    }
-
-    // Public: turn off drag tracking for this dragTracker object.
-    function destroy() {
-        $(handle).off('mousedown', mouseDown);
-    }
-
-    $(handle).on('mousedown', mouseDown);
-
-    return {destroy: destroy};
-};
-
-
-// resizer is a component that uses a dragTracker under the hood to track the
-// dragging of a handle element, using that motion to resize another element.
-//
-// element - DOM Element to resize
-// handle - DOM Element to use as a resize handle
-// options - Object of options.
-//
-// Available options:
-//
-// invertedX - If this option is defined as a function, and that function
-//             returns a truthy value, the horizontal sense of the drag will be
-//             inverted. Useful if the drag handle is at the left of the
-//             element, and so dragging left means "grow the element"
-// invertedY - If this option is defined as a function, and that function
-//             returns a truthy value, the vertical sense of the drag will be
-//             inverted. Useful if the drag handle is at the bottom of the
-//             element, and so dragging down means "grow the element"
-var resizer = exports.resizer = function resizer(element, handle, options) {
-    var $el = $(element);
-    if (typeof options === 'undefined' || options === null) {
-        options = {};
-    }
-
-    // Translate the delta supplied by dragTracker into a delta that takes
-    // account of the invertedX and invertedY callbacks if defined.
-    function translate(delta) {
-        var directionX = 1,
-            directionY = -1;
-
-        if (typeof options.invertedX === 'function' && options.invertedX()) {
-            directionX = -1;
-        }
-        if (typeof options.invertedY === 'function' && options.invertedY()) {
-            directionY = 1;
-        }
-
-        return {
-            x: delta.x * directionX,
-            y: delta.y * directionY
-        };
-    }
-
-    // Callback for dragTracker
-    function resize(delta) {
-        var height = $el.height(),
-            width = $el.width(),
-            translated = translate(delta);
-
-        if (Math.abs(translated.x) > 0) {
-            $el.width(width + translated.x);
-        }
-        if (Math.abs(translated.y) > 0) {
-            $el.height(height + translated.y);
-        }
-
-        // Did the element dimensions actually change? If not, then we've
-        // reached the minimum size, and we shouldn't track
-        var didChange = ($el.height() !== height || $el.width() !== width);
-        return didChange;
-    }
-
-    // We return the dragTracker object in order to expose its methods.
-    return dragTracker(handle, resize);
-};
-
-
-// mover is a component that uses a dragTracker under the hood to track the
-// dragging of a handle element, using that motion to move another element.
-//
-// element - DOM Element to move
-// handle - DOM Element to use as a move handle
-//
-var mover = exports.mover = function mover(element, handle) {
-    function move(delta) {
-        $(element).css({
-            top: parseInt($(element).css('top'), 10) + delta.y,
-            left: parseInt($(element).css('left'), 10) + delta.x
-        });
-    }
-
-    // We return the dragTracker object in order to expose its methods.
-    return dragTracker(handle, move);
-};
-
 
 // Public: Creates an element for editing annotations.
-var Editor = exports.Editor = Widget.extend({
+var ddiEditor = exports.Editor = Editor.extend({
     // Public: Creates an instance of the Editor object.
-    //
-    // options - An Object literal containing options.
-    //
-    // Examples
-    //
-    //   # Creates a new editor, adds a custom field and
-    //   # loads an annotation for editing.
-    //   editor = new Annotator.Editor
-    //   editor.addField({
-    //     label: 'My custom input field',
-    //     type:  'textarea'
-    //     load:  someLoadCallback
-    //     save:  someSaveCallback
-    //   })
-    //   editor.load(annotation)
-    //
-    // Returns a new Editor instance.
+
     constructor: function (options) {
-        Widget.call(this, options);
+        Editor.call(this, options);
 
         this.fields = [];
         this.annotation = {};
@@ -244,17 +33,17 @@ var Editor = exports.Editor = Widget.extend({
                 }
             });
 
-	    // add new field as part of default - drug name
-	    // this.addField({
-	    // 	label: _t('Drug name') + '\u2026',
-	    // 	type:  'textarea',
-	    // 	load: function (field, annotation) {
-	    // 	    $(field).find('#annotator-field-1').val(annotation.drug || '');
-	    // 	},
-	    // 	submit: function (field, annotation){
-	    // 	    annotation.drug = $(field).find('#annotator-field-1').val();
-	    // 	} 
-	    // });
+	    add new field as part of default - drug name
+	    this.addField({
+	    	label: _t('Drug name') + '\u2026',
+	    	type:  'textarea',
+	    	load: function (field, annotation) {
+	    	    $(field).find('#annotator-field-1').val(annotation.drug || '');
+	    	},
+	    	submit: function (field, annotation){
+	    	    annotation.drug = $(field).find('#annotator-field-1').val();
+	    	} 
+	    });
 
 	    // this.addField({
 	    // 	type: 'checkbox',
@@ -596,46 +385,3 @@ var Editor = exports.Editor = Widget.extend({
         this._mover = mover(this.element[0], controls);
     }
 });
-
-// Classes to toggle state.
-Editor.classes = {
-    hide: 'annotator-hide',
-    focus: 'annotator-focus'
-};
-
-// HTML template for this.element.
-Editor.template = [
-    '<div class="annotator-outer annotator-editor annotator-hide">',
-    '  <form class="annotator-widget">',
-    '    <ul class="annotator-listing"></ul>',
-    '    <div class="annotator-controls">',
-    '     <a href="#cancel" class="annotator-cancel">' + _t('Cancel') + '</a>',
-    '      <a href="#save"',
-    '         class="annotator-save annotator-focus">' + _t('Save') + '</a>',
-    '    </div>',
-    '  </form>',
-    '</div>'
-].join('\n');
-
-// Configuration options
-Editor.options = {
-    // Add the default field(s) to the editor.
-    defaultFields: true
-};
-
-// standalone is a module that uses the Editor to display an editor widget
-// allowing the user to provide a note (and other data) before an annotation is
-// created or updated.
-exports.standalone = function standalone(options) {
-    var widget = new exports.Editor(options);
-
-    return {
-        destroy: function () { widget.destroy(); },
-        beforeAnnotationCreated: function (annotation) {
-            return widget.load(annotation);
-        },
-        beforeAnnotationUpdated: function (annotation) {
-            return widget.load(annotation);
-        }
-    };
-};
